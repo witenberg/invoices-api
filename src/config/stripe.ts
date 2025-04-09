4916e1d00ba4a3c960048d3c6068e39d0bb7a661
witenberg <jakub.witenberg@gmail.com>
2025-04-09 22:29:42 +0200

    stripe config fix

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,6 +1,6 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-export const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+export const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY!);
 
 export const stripePromise = loadStripe(
   process.env.STRIPE_PUBLISHABLE_KEY!
